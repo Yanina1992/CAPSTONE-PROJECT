51fab9a42ce54e14b0191a811ef903fa710ec247
Yanina Agüero <132350660+Yanina1992@users.noreply.github.com>
2023-11-06 18:30:35 +0100

    Gestione classi e pagamenti

--- CAPSTONE-PROJECT/Controllers/AreaRiservataController.cs
+++ CAPSTONE-PROJECT/Controllers/AreaRiservataController.cs
@@ -24,7 +24,7 @@ namespace CAPSTONE_PROJECT.Controllers
         List<DomandeIscrizione> alunniPrimeInEccesso = new List<DomandeIscrizione>();
         List<DomandeIscrizione> alunniSecondeInEccesso = new List<DomandeIscrizione>();
         List<DomandeIscrizione> alunniTerzeInEccesso = new List<DomandeIscrizione>();
-        
+
         List<Classi> listaClassi = new List<Classi>();
 
         // GET: AreaRiservata
@@ -145,27 +145,23 @@ namespace CAPSTONE_PROJECT.Controllers
             return View(classiTerzeTC.ToList());
         }
 
-        //COMMENTO PER RINO:
-            //Con l'Action Result "SalvaClassi", in GET voglio stampare la listaClassi (lo faccio con una ViewBag, e funziona correttamente);
-            //in POST, invece, vorrei salvare con un'iterazione le Classi che ho nella lista sul db, ma non ci riesco.
-            //Il metodo "SalvaClassi" dovrebbe essere innescato dall'action link che si trova nella visualizzazione del metodo GestioneClassi di questo stesso controller.
-        public ActionResult SalvaClassi()
+        public JsonResult SalvaClassi()
         {
             GestioneClassi();
-            
-            if (classiPrimeTP.Count > 5) 
+
+            if (classiPrimeTP.Count > 5)
             {
                 Classi PrimaA = new Classi();
-                PrimaA.Anno = "Prima";
+                PrimaA.Anno = "1";
                 PrimaA.Sezione = "A";
 
-                listaClassi.Add( PrimaA );
+                listaClassi.Add(PrimaA);
             }
 
             if (classiPrimeTC.Count > 5)
             {
                 Classi PrimaB = new Classi();
-                PrimaB.Anno = "Prima";
+                PrimaB.Anno = "1";
                 PrimaB.Sezione = "B";
 
                 listaClassi.Add(PrimaB);
@@ -174,7 +170,7 @@ namespace CAPSTONE_PROJECT.Controllers
             if (classiSecondeTP.Count > 5)
             {
                 Classi SecondaA = new Classi();
-                SecondaA.Anno = "Seconda";
+                SecondaA.Anno = "2";
                 SecondaA.Sezione = "A";
 
                 listaClassi.Add(SecondaA);
@@ -183,7 +179,7 @@ namespace CAPSTONE_PROJECT.Controllers
             if (classiSecondeTC.Count > 5)
             {
                 Classi SecondaB = new Classi();
-                SecondaB.Anno = "Seconda";
+                SecondaB.Anno = "2";
                 SecondaB.Sezione = "B";
 
                 listaClassi.Add(SecondaB);
@@ -192,7 +188,7 @@ namespace CAPSTONE_PROJECT.Controllers
             if (classiTerzeTP.Count > 5)
             {
                 Classi TerzaA = new Classi();
-                TerzaA.Anno = "Terza";
+                TerzaA.Anno = "3";
                 TerzaA.Sezione = "A";
 
                 listaClassi.Add(TerzaA);
@@ -201,150 +197,36 @@ namespace CAPSTONE_PROJECT.Controllers
             if (classiTerzeTC.Count > 5)
             {
                 Classi TerzaB = new Classi();
-                TerzaB.Anno = "Terza";
+                TerzaB.Anno = "3";
                 TerzaB.Sezione = "B";
 
                 listaClassi.Add(TerzaB);
             }
 
-            Session["ClassiGenerate"] = listaClassi;
-            ViewBag.classi = listaClassi;
-           
-            return View();
+            return Json(listaClassi, JsonRequestBehavior.AllowGet);
         }
 
-        [HttpPost]
-        public ActionResult SalvaClassi(Classi classe)
+        public JsonResult SalvaC()
         {
-            var listaDaCiclare = Session["ClassiGenerate"];
-            //foreach (var classi in listaDaCiclare as Array)
-            foreach (var classi in listaClassi)
+            GestioneClassi();
+            SalvaClassi();
+            foreach (var item in listaClassi)
             {
-            //    Classi classe = new Classi();
-            //    if (ModelState.IsValid)
-            //    {   
-            //        db.Classi.Add(classe);
-            //    }
-            //    try
-            //    {
-            //    db.SaveChanges();
-            //    ViewBag.Message = "Classi salvate con successo";
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //    ViewBag.Message = ex.Message;
-            //    }
-           
-            db.Classi.Add(classi);
-            db.SaveChanges();
-            return View();
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                    db.Classi.Add(item);
+                    db.SaveChanges();
+                    }
+                    catch (Exception ex) 
+                    {
+                        ViewBag.Error = ex.Message;
+                    }
+                }
             }
 
-            return View();
+            return Json(Response, JsonRequestBehavior.AllowGet);
         }
-                //foreach (var item in classiPrimeTP)
-                //{
-                //    //item.DomandaAccolta = true;
-                //    Alunni alunno1A = new Alunni();
-                //    alunno1A.FKDomandaIscrizione = item.IdDomanda;
-                //    alunno1A.FKClasse = PrimaA.IdClasse;
-
-                //    if (ModelState.IsValid)
-                //    {
-                //        alunno1A = alunno;
-                //        db.Alunni.Add(alunno);
-
-                //        db.SaveChanges();
-                //    }
-                //}
-            }
-
-        }
-               
-
-//            if (classiPrimeTC.Count > 5)
-//            {
-//                Classi PrimaB = new Classi();
-//                PrimaB.Anno = "Prima";
-//                PrimaB.Sezione = "B";
-//                db.Classi.Add(PrimaB);
-
-//                foreach (var item in classiPrimeTC)
-//                {
-//                    item.DomandaAccolta = true;
-//                    Alunni alunno = new Alunni();
-//                    db.Alunni.Add(alunno);
-//                }
-
-//            }
-
-//            if (classiSecondeTP.Count > 5)
-//            {
-//                Classi SecondaA = new Classi();
-//                SecondaA.Anno = "Seconda";
-//                SecondaA.Sezione = "A";
-//                db.Classi.Add(SecondaA);
-
-//                foreach (var item in classiSecondeTP)
-//                {
-//                    item.DomandaAccolta = true;
-//                    Alunni alunno = new Alunni();
-//                    db.Alunni.Add(alunno);
-//                }
-
-//            }
-
-//            if (classiSecondeTC.Count > 5)
-//            {
-//                Classi SecondaB = new Classi();
-//                SecondaB.Anno = "Seconda";
-//                SecondaB.Sezione = "B";
-//                db.Classi.Add(SecondaB);
-
-//                foreach (var item in classiSecondeTC)
-//                {
-//                    item.DomandaAccolta = true;
-//                    Alunni alunno = new Alunni();
-//                    db.Alunni.Add(alunno);
-//                }
-
-//            }
-
-//            if (classiTerzeTP.Count > 5)
-//            {
-//                Classi TerzaA = new Classi();
-//                TerzaA.Anno = "Terza";
-//                TerzaA.Sezione = "A";
-//                db.Classi.Add(TerzaA);
-
-//                foreach (var item in classiTerzeTP)
-//                {
-//                    item.DomandaAccolta = true;
-//                    Alunni alunno = new Alunni();
-//                    db.Alunni.Add(alunno);
-//                }
-
-//            }
-
-//            if (classiTerzeTC.Count > 5)
-//            {
-//                Classi TerzaB = new Classi();
-//                TerzaB.Anno = "Terza";
-//                TerzaB.Sezione = "B";
-//                db.Classi.Add(TerzaB);
-
-//                foreach (var item in classiTerzeTC)
-//                {
-//                    item.DomandaAccolta = true;
-//                    Alunni alunno = new Alunni();
-//                    db.Alunni.Add(alunno);
-//                }
-
-//            }
-
-//            db.SaveChanges();
-
-//            return View();
-//        }
-//    }
-//}
+    }
+}            
--- CAPSTONE-PROJECT/Controllers/FamiglieController.cs
+++ CAPSTONE-PROJECT/Controllers/FamiglieController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CAPSTONE_PROJECT.Models;
+using Microsoft.Ajax.Utilities;
 
 namespace CAPSTONE_PROJECT.Controllers
 {
@@ -15,9 +18,130 @@ namespace CAPSTONE_PROJECT.Controllers
             return View();
         }
 
-        public ActionResult TrovaPagamenti(string CodiceFiscale) 
+        public ActionResult TrovaPagamenti() 
         {
             return View();
+        }
+
+        public JsonResult Pagamento(string cf)
+        {
+            DomandeIscrizione domanda = db.DomandeIscrizione.FirstOrDefault(e => e.CFPapa == cf || e.CFMamma == cf);
+            try
+            {
+                Alunni alunno = db.Alunni.Where(m => m.FKDomandaIscrizione.ToString() == domanda.IdDomanda.ToString()).FirstOrDefault();
+
+                var bilinguismo = domanda.Bilinguismo;
+                var assicurazione = domanda.Assicurazione;
+                var isee = domanda.Isee;
+                var mensa = domanda.Mensa;
+                var trasportoScolastico = domanda.TrasportoScolastico;
+                decimal totale = 0;
+
+                Pagamenti pagamenti = new Pagamenti();
+
+                if (bilinguismo == true)
+                {
+                    pagamenti.Bilinguismo = 450;
+                    totale += 450;
+                }
+                if (assicurazione == true)
+                {
+                    pagamenti.Assicurazione = 6;
+                    totale += 6;
+                }
+
+                if (isee < 23120)
+                {
+                    if (mensa == true) 
+                    {
+                        pagamenti.Mensa = 30;
+                        totale += 30;
+                    }
+                    if (trasportoScolastico == true)
+                    {
+                        pagamenti.TrasportoScolastico = 15;
+                        totale += 15;
+                    }
+                }
+                else if (isee > 23121 && isee < 27000)
+                {
+                    if (mensa == true)
+                    {
+                        pagamenti.Mensa = 40;
+                        totale += 40;
+                    }
+                    if (trasportoScolastico == true)
+                    {
+                        pagamenti.TrasportoScolastico = 20;
+                        totale += 20;
+                    }
+                }
+                else if (isee > 27001 && isee < 31000)
+                {
+                    if (mensa == true)
+                    {
+                        pagamenti.Mensa = 50;
+                        totale += 50;
+                    }
+                    if (trasportoScolastico == true)
+                    {
+                        pagamenti.TrasportoScolastico = 25;
+                        totale += 25;
+                    }
+                }
+                else if (isee > 31001 && isee > 40000)
+                {
+                    if (mensa == true)
+                    {
+                        pagamenti.Mensa = 60;
+                        totale += 60;
+                    }
+                    if (trasportoScolastico == true)
+                    {
+                        pagamenti.TrasportoScolastico = 30;
+                        totale += 30;
+                    }
+                }
+                else if (isee > 41000)
+                {
+                    if (mensa == true)
+                    {
+                        pagamenti.Mensa = 70;
+                        totale += 70;
+                    }
+                    if (trasportoScolastico == true)
+                    {
+                        pagamenti.TrasportoScolastico = 35;
+                        totale += 35;
+                    }
+                }
+                else
+                {
+                    Exception exception = new Exception();
+                }
+
+                pagamenti.Totale = totale;
+
+                if (ModelState.IsValid)
+                {
+                    db.Pagamenti.Add( pagamenti );
+                    db.SaveChanges();
+                }
+
+                alunno.FKPagamento = pagamenti.IdPagamento;
+
+                if (ModelState.IsValid)
+                {
+                    db.Entry(alunno).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+
+            }
+            catch (Exception ex) 
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return Json(Response, JsonRequestBehavior.AllowGet);
         }
     }
 }
